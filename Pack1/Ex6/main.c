2db66cefa6ec27186cff270ba636778a6dfe3a4d
Vika <vika.yefremova.06@mail.ru>
2025-10-09 17:26:36 +0300

    aaaa

---
+++ Pack1/Ex6/main.c
@@ -0,0 +1,45 @@
+#include "include/functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char ** argv) {
+    if (argc != 2) {
+        printf("Wrong number of args!\n");
+        return 0;
+    }
+
+    char * str[BUFSIZ];
+    double eps = strtod(argv[1], str);
+    if (!eps) {
+        printf("Something went wrong\n");
+        return 0;
+    } 
+    if (eps < 0) {
+        printf("Epsilon must be positive!\n");
+        return 0;
+    }
+    if (eps < 1e-17) {
+        printf("too small epsilon!\n");
+        return 0;
+    }
+
+    double a = 0;
+    double b = 1;
+    double res = 0;
+    ReturnCode return_code;
+
+    double (*functions[4])(double) = {FuncA, FuncB, FuncC, FuncD};
+
+    for (int i = 1; i < 5; ++i) {
+        return_code = trapezoidMethod(eps, a, b, &res, functions[i - 1]);
+        if (return_code == OK) {
+            printf("Integral %i: %f\n", i, res);
+        }
+        if (return_code == NULL_POINTER) {
+            printf("Come across a null poiter\n");
+        }
+    }
+    return 0;
+
+}
